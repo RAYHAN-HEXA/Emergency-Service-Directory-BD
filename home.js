1b1f1dbb040cd28f051a2e6753edd82ff1654c41
RAYHAN-HEXA <mohammadrayahn796@gmail.com>
2025-08-28 11:06:01 +0600

    update code

--- home.js
+++ home.js
@@ -1,4 +1,3 @@
-
 let heartCount = 0;
 let coinCount = 100;
 let copyCount = 0;
@@ -12,3 +11,23 @@ const clearButton = document.querySelector(".clear-history");
 const heartButtons = document.querySelectorAll(".heart-btn");
 const copyButtons = document.querySelectorAll(".copy-btn");
 const callButtons = document.querySelectorAll(".call-btn");
+
+// Heart Button
+heartButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    heartCount++;
+    heartDisplay.textContent = heartCount;
+  });
+});
+
+// Copy Button
+copyButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const card = button.parentElement.parentElement;
+    const number = card.getAttribute("data-number");
+    navigator.clipboard.writeText(number);
+    alert("Copied: " + number);
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });
+});
